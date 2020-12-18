6ccd22ca0cea48f2085327a55e1daf4deaf9c1e0
Marc-André Jolicoeur <marc-andre.jolicoeur@polymtl.ca>
2020-12-18 01:25:42 -0500

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainPage from './scenes/MainPage';
 import Temps from './scenes/Temps';
-// import { NavBar, Footer } from '@components';
+
 
 import './App.css';
 
 const App = () => {
     return (
-            /* <NavBar /> */
-            <Switch>
-                <Route exact path="/" component={MainPage} />
-                <Route exact path="/signIn" component={Temps} />
-            </Switch>
-            /* <Footer /> */
+            <Router basename={process.env.PUBLIC_URL}>
+                <Switch>
+                    <Route exact path="/" component={MainPage} />
+                    <Route exact path="/signIn" component={Temps} />
+                </Switch>
+            </Router>
     );
 };
 
